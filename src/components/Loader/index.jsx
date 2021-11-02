943230dc6de3b34647304318dc23f1002973373f
Sd <sdevone@outlook.fr>
2021-11-02 18:08:05 +0100

    addition of the loader component

---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+import "../../styles/loader.css"
+
+/**
+ * renders a loading spinner
+ * @component
+ */
+class Loader extends Component {
+  render() {
+    return <div className="loader"></div>
+  }
+}
+
+export default Loader
