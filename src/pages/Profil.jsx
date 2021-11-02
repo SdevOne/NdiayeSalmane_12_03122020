44d8040c3ec355a7df423e90b6563b5355e5ba91
Sd <sdevone@outlook.fr>
2021-11-02 18:10:22 +0100

    addition of the profil page component

---
+++ src/pages/Profil.jsx
@@ -0,0 +1,101 @@
+import React, { Component } from "react"
+import { getUser } from "../api"
+import BarChartComp from "../components/Chart/BarChartComp"
+import LineChartComp from "../components/Chart/LineChartComp"
+import RadarChartComp from "../components/Chart/RadarChartComp"
+import RadialBarChart from "../components/Chart/RadialChartComp"
+import Nutrient from "../components/Nutrient"
+import energy from "../assets/energy.svg"
+import chicken from "../assets/chicken.svg"
+import apple from "../assets/apple.svg"
+import cheeseburger from "../assets/cheeseburger.svg"
+import "../styles/profil.css"
+
+/**
+ * Renders the user profil page
+ * @component
+ */
+class Profil extends Component {
+  state = { data: {}, isLoading: true }
+
+  componentDidMount() {
+    this._isMounted = true
+    const { id } = this.props.match.params
+    getUser(id).then((result) => {
+      if (!result.data) {
+        this.props.history.push("/Error")
+      } else {
+        this.setState({ data: result.data })
+        this.setState({ isLoading: false })
+      }
+    })
+  }
+
+  render() {
+    const { id } = this.props.match.params
+    const { data, isLoading } = this.state
+    return (
+      <>
+        {!isLoading ? (
+          <main className="profil">
+            <div className="profil__intel">
+              <h1 className="profil__header">
+                Bonjour
+                <span className="profil__name">
+                  {" " +
+                    data.userInfos.lastName +
+                    " " +
+                    data.userInfos.firstName}
+                </span>
+              </h1>
+              <p className="profil__appreciation">
+                Félicitation ! Vous avez explosé vos objectifs hier 👏
+              </p>
+              <section className="profil__charts-container">
+                <div className="profil__charts">
+                  <div className="profil__barChart">
+                    <BarChartComp id={id} />
+                  </div>
+                  <div className="profil__sportChart">
+                    <LineChartComp id={id} />
+                    <RadarChartComp id={id} />
+                    <RadialBarChart
+                      score={data.todayScore ? data.todayScore : data.score}
+                    />
+                  </div>
+                </div>
+                <aside className="profil__nutrients-container">
+                  <Nutrient
+                    count={data.keyData.calorieCount + "Cal"}
+                    nutrient="Calories"
+                    logo={energy}
+                    background="red-bckg"
+                  />
+                  <Nutrient
+                    count={data.keyData.proteinCount + "g"}
+                    nutrient="Proteines"
+                    logo={chicken}
+                    background="blue-bckg"
+                  />
+                  <Nutrient
+                    count={data.keyData.carbohydrateCount + "g"}
+                    nutrient="Glucides"
+                    logo={apple}
+                    background="yellow-bckg"
+                  />
+                  <Nutrient
+                    count={data.keyData.lipidCount + "g"}
+                    nutrient="Lipides"
+                    logo={cheeseburger}
+                    background="pink-bckg"
+                  />
+                </aside>
+              </section>
+            </div>
+          </main>
+        ) : null}
+      </>
+    )
+  }
+}
+export default Profil
