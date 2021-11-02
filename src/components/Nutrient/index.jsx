a39dc91787837f6b2b22e42adbd67289353c616c
Sd <sdevone@outlook.fr>
2021-11-02 18:07:16 +0100

    addition of the nutrient component

---
+++ src/components/Nutrient/index.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react"
+import Icon from "../Icon"
+import PropTypes from "prop-types"
+import "../../styles/nutrient.css"
+
+/**
+ * Renders a nutrient component
+ * @component
+ * @param {string} this.props.logo the icon
+ * @param {("red-bckg"|"blue-bckg"|"yellow-bckg"|"pink-bckg")} this.props.background the css class style of the icon
+ * @param {string} this.props.count the nutrient value
+ * @param {string} this.props.nutrient the nutrient type
+ */
+class Nutrient extends Component {
+  static propTypes = {
+    logo: PropTypes.string,
+    background: PropTypes.string,
+    count: PropTypes.string,
+    nutrient: PropTypes.string,
+  }
+
+  render() {
+    return (
+      <div className="nutrient__container">
+        <div className="nutrient__content">
+          <Icon logo={this.props.logo} background={this.props.background} />
+          <div className="nutrient__intel">
+            <p className="nutrient__average">{this.props.count}</p>
+            <p className="nutrient__type">{this.props.nutrient}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default Nutrient
